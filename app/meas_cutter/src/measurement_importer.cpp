b543f09c3b6700a0e94cc299dbf7e92da35671cd
Chirila Adriana <26360943+adriana-chirila@users.noreply.github.com>
2022-08-19 15:08:52 +0300

    Update measurement_importer.cpp

--- app/meas_cutter/src/measurement_importer.cpp
+++ app/meas_cutter/src/measurement_importer.cpp
@@ -141,7 +141,7 @@ std::pair<eCALMeasCutterUtils::Timestamp, eCALMeasCutterUtils::Timestamp> Measur
 std::list<std::string> MeasurementImporter::getChannelNamesForRegex(const std::regex& regex)
 {
   std::list<std::string> channel_names_matches;
-  for (auto channel_name : _channel_names)
+  for (const auto& channel_name : _channel_names)
   {
     if (std::regex_match(channel_name, regex))
     {
@@ -189,7 +189,7 @@ std::string MeasurementImporter::getLoadedPath()
 
 bool MeasurementImporter::areChannelsLeftToInclude(const std::list<std::string>& channels_to_exclude)
 {
-  for (auto channel_name : _channel_names)
+  for (const auto& channel_name : _channel_names)
   {
     // if we have at least a channel that is not excluded
     if (std::find(channels_to_exclude.begin(), channels_to_exclude.end(), channel_name) == channels_to_exclude.end())
