2f0595c364c58f42dfceed5fe9e6ef6645de4f49
Chirila Adriana <26360943+adriana-chirila@users.noreply.github.com>
2022-08-21 10:26:58 +0300

    Update measurement_importer.h

--- app/meas_cutter/src/measurement_importer.h
+++ app/meas_cutter/src/measurement_importer.h
@@ -59,7 +59,7 @@ class ImporterException : public std::exception
 public:
   ImporterException(const std::string& message) : message_(message) {}
 
-  virtual const char* what() const throw() { return message_.c_str(); }
+  const char* what() const throw() override { return message_.c_str(); }
 private:
   std::string message_;
 };
